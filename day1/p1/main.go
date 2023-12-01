62781d039fb7d91d68188d69437c98ab5d152076
Alexey Vekshin <alexei.vekshin@gmail.com>
2023-12-01 10:51:29 +0000

    day1 ok

--- day1/p1/main.go
+++ day1/p1/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"regexp"
 )
 
 const INPUT = "../input.txt"
@@ -15,10 +16,18 @@ func main() {
 		log.Fatalf("error opening file: %v", err)
 	}
 	defer f.Close()
+	re := regexp.MustCompile(`\d`)
 	scanner := bufio.NewScanner(f)
+	res := 0
 	for scanner.Scan() {
 		line := scanner.Text()
-		log.Printf("got %s", line)
+		// log.Printf("got %s", line)
+		digits := re.FindAllString(line, -1)
+		// alt with re1 := regexp.MustCompile(`(\d).*(\d)`)
+		// ss := re1.FindStringSubmatch("p1a2ra33no4a")
+		// first, last := ss[1], ss[2]
+		first, last := int(digits[0][0] - '0'), int(digits[len(digits)-1][0] - '0')
+		res += first*10 + last
 	}
-	log.Print("done")
+	log.Printf("done, res: %d", res)
 }
