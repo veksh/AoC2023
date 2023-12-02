19a698170dd94ddce2c9c0222d30f0f3c271de49
Alexey Vekshin <alexei.vekshin@gmail.com>
2023-12-02 22:42:28 +0000

    some comments after reading other's solutions

--- day2/solve2.py
+++ day2/solve2.py
@@ -6,6 +6,9 @@ from functools import reduce
 
 FILE_NAME = "input.txt"
 
+# usual way:
+# import sys; D = open(sys.argv[1]).read().strip(); for line in D.split('\n'):
+
 res = 0
 for line in open(FILE_NAME).read().splitlines():
   print("got", line)
@@ -14,13 +17,15 @@ for line in open(FILE_NAME).read().splitlines():
   stillOK = True
   mc = defaultdict(int)
   for game_set in line[line.index(": ")+2:].split('; '):
+    # colors = [x.split() for x in set.split(", ")]
+    # counts = {b: int(a) for a, b in colors}
+    # if not (counts.get("red", 0) <= 12 ...
     print("    game set: %s" % game_set)
     for pair in game_set.split(", "):
       qty, color = pair.split(" ")
       qty = int(qty)
       print("      pair: %s -> %d" %(color, qty))
-      if mc[color] < qty:
-        mc[color] = qty
+      mc[color] = max(mc[color], qty)
   res += reduce(lambda s, x: s*x, mc.values())
 
 print("result: %d" % res)
