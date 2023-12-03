c2da9cc3407b03a5bd20bb460f46c2e9911ac99b
Alexey Vekshin <alexei.vekshin@gmail.com>
2023-12-03 19:04:43 +0000

    part1 done

--- day3/p1.rb
+++ day3/p1.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby
 
-FILE_NAME = "input_test.txt"
+FILE_NAME = "input.txt"
 
 def l2numpos(l)
   return [[0, 1]]
@@ -12,9 +12,9 @@ end
 
 lines = IO.read(FILE_NAME).lines().map(&:chomp)
 res = 0
-lines.each_with_index do |l, i|
+lines.each_with_index do |l, li|
   matches = l.gsub(/\d+/).map{ Regexp.last_match }.map {|m| [m[0].to_i, m.begin(0), m.end(0)]}
-  puts "#{sprintf('%3d', i)}: #{l}: #{matches}"
+  puts "#{sprintf('%3d', li)}: #{l}: #{matches}"
   matches.each do |m|
     puts " studying #{m}"
     case
@@ -22,11 +22,16 @@ lines.each_with_index do |l, i|
       puts "  match before"
     when m[2] < l.length()-1 && l[m[2]] != '.'
       puts "  match after"
+    when li > 0 && !lines[li-1][[m[1]-1,0].max..[m[2],l.length()-1].min].match?(/^[0-9.]+$/)
+      puts "  match above"
+    when li < lines.length()-1 && !lines[li+1][[m[1]-1,0].max..[m[2],l.length()-1].min].match?(/^[0-9.]+$/)
+      puts "  match below"
     else
       puts "  no match"
       next
     end
-      puts "    adding #{m[0]}"
-      res += m[0]
+    puts "    adding #{m[0]}"
+    res += m[0]
   end
+  puts "result: #{res}"
 end
