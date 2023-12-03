1ef541528ea9cc3c90430b8accfe7b73e3619161
Alexey Vekshin <alexei.vekshin@gmail.com>
2023-12-03 18:43:09 +0000

    matches before and after

--- day3/p1.rb
+++ day3/p1.rb
@@ -13,6 +13,20 @@ end
 lines = IO.read(FILE_NAME).lines().map(&:chomp)
 res = 0
 lines.each_with_index do |l, i|
-  m = l.gsub(/\d+/).map{ Regexp.last_match }.map {|m| [m[0].to_i, m.begin(0), m.end(0)]}
-  puts "#{sprintf('%3d', i)}: #{l}: #{m}"
+  matches = l.gsub(/\d+/).map{ Regexp.last_match }.map {|m| [m[0].to_i, m.begin(0), m.end(0)]}
+  puts "#{sprintf('%3d', i)}: #{l}: #{matches}"
+  matches.each do |m|
+    puts " studying #{m}"
+    case
+    when m[1] > 0 && l[m[1]-1] != '.'
+      puts "  match before"
+    when m[2] < l.length()-1 && l[m[2]] != '.'
+      puts "  match after"
+    else
+      puts "  no match"
+      next
+    end
+      puts "    adding #{m[0]}"
+      res += m[0]
+  end
 end
