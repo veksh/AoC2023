7b74654a6246e75ef4f8ba352394fbc99bc1ec40
Alexey Vekshin <alexei.vekshin@gmail.com>
2023-12-04 09:41:22 +0000

    d4.p2 ok

--- day4/p2.py
+++ day4/p2.py
@@ -3,12 +3,15 @@
 # Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 FILE_NAME = "input.txt"
 
-res = 0
+qty = []
 for line in open(FILE_NAME).read().splitlines():
   cardNo = int(line[5:line.index(':')])
   winning, ours = [set(map(int, n.split())) for n in line[line.index(':')+1:].split('|')]
-  print("line %3d: winning %s ours %s" % (cardNo, winning, ours))
   wins = len(winning & ours)
-  if wins > 0:
-    res += 2**(wins-1)
-print("result:", res)
+  print("card %3d: winning %s ours %s wins %d" % (cardNo, winning, ours, wins))
+  if len(qty) < cardNo + wins:
+    qty += [1] * (cardNo + wins - len(qty))
+  for i in range(0, wins):
+    qty[cardNo + i] += qty[cardNo-1]
+  print(" qty", qty)
+print("result: ", sum(qty))
