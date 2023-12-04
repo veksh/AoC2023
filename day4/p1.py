60a120130d1ddecb7c4a6aff2cced1c6f7321c70
Alexey Vekshin <alexei.vekshin@gmail.com>
2023-12-04 09:10:39 +0000

    d4.p1 ok

--- day4/p1.py
+++ day4/p1.py
@@ -3,9 +3,14 @@
 import re
 
 # Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
-FILE_NAME = "input_test.txt"
+FILE_NAME = "input.txt"
 
+res = 0
 for line in open(FILE_NAME).read().splitlines():
   cardNo = int(line[5:line.index(':')])
   winning, ours = [set(map(int, n.split())) for n in line[line.index(':')+1:].split('|')]
   print("line %3d: winning %s ours %s" % (cardNo, winning, ours))
+  wins = len(winning & ours)
+  if wins > 0:
+    res += 2**(wins-1)
+print("result:", res)
