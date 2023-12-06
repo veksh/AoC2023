0cd3e1ec7dd8e267ad25c89928f12761abfb46f7
Alexey Vekshin <alexei.vekshin@gmail.com>
2023-12-06 08:51:46 +0000

    d6.p1 ok

--- day6/p1.rb
+++ day6/p1.rb
@@ -1,5 +1,13 @@
 #!/usr/bin/ruby
 
-td = ARGF.readlines().map {|l| l.split()[1..]}
-puts "times: #{td[0]}, distances: #{td[1]}"
+td = ARGF.readlines().map {|l| l.split()[1..].map(&:to_i)}
+puts "times: #{td[0]}, records: #{td[1]}"
 
+res = 1
+td[0].zip(td[1]).each do |p|
+  dist = (1..p[0]).map {|h| h*(p[0]-h)}
+  cnt   = dist.count {|d| d > p[1]}
+  puts "time #{p[0]} record #{p[1]}: #{cnt} ways (dist: #{dist})"
+  res *= cnt
+end
+puts "ans: #{res}"
