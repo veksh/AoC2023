5fdfcd9530a25ac7c1b29516bbd0ec44e33a40c6
Alexey Vekshin <alexei.vekshin@gmail.com>
2023-12-07 09:58:13 +0000

    add card values

--- day7/p1.py
+++ day7/p1.py
@@ -5,4 +5,6 @@ import sys
 # "0" for stdin
 lines = open(sys.argv[1] if len(sys.argv) > 1 else "input_test.txt").read().splitlines()
 bets = {k: int(v) for k,v in [p.split() for p in lines]}
+cards = {l: i for i, l in enumerate(reversed("A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, 2".split(", ")))}
 print(bets)
+print(cards)
