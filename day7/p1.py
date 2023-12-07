0a2d8b690b2736e33653c84c97a7cc74326923bc
Alexey Vekshin <alexei.vekshin@gmail.com>
2023-12-07 10:44:49 +0000

    d7.p1 ok

--- day7/p1.py
+++ day7/p1.py
@@ -15,8 +15,17 @@ combo_val = {
   (1, 1, 3):       3,  # three of a kind
   (2, 3):          4,  # full house
   (1, 4):          5,  # four of a kind
-  (5):             6   # five of a kind
+  (5, ):           6   # five of a kind
 }
+
+card2val = lambda card: \
+  chr(ord('A') + combo_val[tuple(sorted(Counter([l for l in card]).values()))]) + \
+  "".join([chr(ord('A') + label_val[c]) for c in card])
+
 print(bets)
 print(label_val)
 print(combo_val)
+res = 0
+for i, c in enumerate(sorted(bets.keys(), key=card2val)):
+  res += (i+1)*bets[c]
+print("answer:", res)
