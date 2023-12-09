dbd6bc76811b670349561407b92a621d315c6e14
Alexey Vekshin <alexei.vekshin@gmail.com>
2023-12-09 13:56:18 +0000

    cleanup

--- day9/p1.rb
+++ day9/p1.rb
@@ -10,15 +10,4 @@ def predict(s)
 end
 
 td = ARGF.readlines().map {|l| l.split().map(&:to_i)}
-# td.each{|l| puts predict(l)}
 puts td.map {|l| predict(l)}.sum()
-
-
-# res = 1
-# td[0].zip(td[1]).each do |p|
-#   dist = (1..p[0]).map {|h| h*(p[0]-h)}
-#   cnt   = dist.count {|d| d > p[1]}
-#   puts "time #{p[0]} record #{p[1]}: #{cnt} ways (dist: #{dist})"
-#   res *= cnt
-# end
-# puts "ans: #{res}"
