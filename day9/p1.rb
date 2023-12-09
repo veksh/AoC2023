1f5aa94608c8d92ded7867f34a2522558660b716
Alexey Vekshin <alexei.vekshin@gmail.com>
2023-12-09 15:24:55 +0000

    d9.p2 and day9 ok

--- day9/p1.rb
+++ day9/p1.rb
@@ -9,5 +9,16 @@ def predict(s)
   return res
 end
 
+def predict2(s)
+  firsts = []
+  while !s.all? {|e| e == 0} do
+    firsts.push(s[0])
+    s = s[1..].zip(s).map{|p| p[0] - p[1]}
+  end
+  return firsts.reverse.reduce(0) {|s, e| e - s}
+end
+
 td = ARGF.readlines().map {|l| l.split().map(&:to_i)}
+
 puts td.map {|l| predict(l)}.sum()
+puts td.map {|l| predict2(l)}.sum()
