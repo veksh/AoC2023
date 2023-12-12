81d42fdc03247ab1bd0ff3cb33a938a034d0e2b3
Alexey Vekshin <alexei.vekshin@gmail.com>
2023-12-12 22:39:19 +0000

    convert pattern to string form, simplified src

--- day12/main.rb
+++ day12/main.rb
@@ -1,3 +1,12 @@
 #!/usr/bin/ruby
-td = ARGF.readlines().map {|l| l.split()}.map {|a| [a[0], a[1].split(',').map(&:to_i)]}
+
+def numVariants(str, strPos, pattern, patterPos)
+  return 1
+end
+
+td = ARGF.readlines().map {|l| l.split()}.map {|a| [a[0].gsub(%r{\.*$|^\.*}, ''), a[1].split(',').map(&:to_i)]}
 puts "#{td}"
+tdt = td.map {|p| [p[0].gsub(%r{\.*$|^\.*}, '').gsub(%r{\.+}, '.'), p[1].map {|n| "#" * n}.join(".")]}
+puts "#{tdt}"
+
+puts numVariants("???.###", 0, "#.#.###", 0)
