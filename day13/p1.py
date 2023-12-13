08d1d3554550c406edafa575e3cc063aa1248d8b
Alexey Vekshin <alexei.vekshin@gmail.com>
2023-12-13 20:25:40 +0000

    word matrix rotation

--- day13/p1.py
+++ day13/p1.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 
 import sys
+import typing
 
 mazes = [p.splitlines() for p in open(sys.argv[1] if len(sys.argv) > 1 else 0).read().split("\n\n")]
-print(mazes)
 
+def rotateMaze(maze: list[str]) -> list[str]:
+  rot = ['' for i in range(0, len(maze[0]))]
+  for lnum,line in enumerate(maze):
+    for cnum,char in enumerate(line):
+      rot[cnum] += char
+  return rot
+
+print("\n".join(mazes[0]))
+print()
+print("\n".join(rotateMaze(mazes[0])))
