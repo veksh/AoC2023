8dbaa7d0e8db521995d66968371fd2bdc82dd085
Alexey Vekshin <alexei.vekshin@gmail.com>
2023-12-13 09:51:27 +0000

    d12.p2 and day 12 ok

--- day12/main.rb
+++ day12/main.rb
@@ -2,10 +2,10 @@
 
 DEBUG = false
 
-def numVariants(str, strPos, pattern, patPos)
-  # if memo.has_key?([strPos, patPos])
-  #   return memo[[strPos, patPos]]
-  # end
+def numVariants(str, strPos, pattern, patPos, memo)
+  if memo.has_key?([strPos, patPos])
+    return memo[[strPos, patPos]]
+  end
   puts "#{' ' * patPos}checking '#{pattern[patPos..]}' on '#{str[strPos..]}' (pos: #{strPos}, #{patPos})" if DEBUG
   ret = -1
   if patPos >= pattern.length()
@@ -23,19 +23,19 @@ def numVariants(str, strPos, pattern, patPos)
   elsif pattern[patPos] == "#"
     case str[strPos]
     when "#"
-      return numVariants(str, strPos + 1, pattern, patPos + 1)
+      return numVariants(str, strPos + 1, pattern, patPos + 1, memo)
     when "."
       # return 0
       if patPos == 0 || pattern[patPos-1] != "#"
-        ret = numVariants(str, strPos + 1, pattern, patPos)
+        ret = numVariants(str, strPos + 1, pattern, patPos, memo)
       else
         puts " #{' ' * patPos}wrong dot" if DEBUG
         ret = 0
       end
     when "?"
-      res = numVariants(str, strPos + 1, pattern, patPos + 1)
+      res = numVariants(str, strPos + 1, pattern, patPos + 1, memo)
       if patPos == 0 || pattern[patPos-1] != "#"
-        res += numVariants(str, strPos + 1, pattern, patPos)
+        res += numVariants(str, strPos + 1, pattern, patPos, memo)
       end
       ret = res
     end
@@ -46,11 +46,12 @@ def numVariants(str, strPos, pattern, patPos)
       puts " #{' ' * patPos}wrong hash" if DEBUG
       ret = 0
     when "."
-      ret = numVariants(str, strPos + 1, pattern, patPos + 1)
+      ret = numVariants(str, strPos + 1, pattern, patPos + 1, memo)
     when "?"
-      ret = numVariants(str, strPos + 1, pattern, patPos + 1)
+      ret = numVariants(str, strPos + 1, pattern, patPos + 1, memo)
     end
   end
+  memo[[strPos, patPos]] = ret
   return ret
 end
 
@@ -58,15 +59,12 @@ td = ARGF.readlines().map {|l| l.split()}.map {|a| [a[0], a[1].split(',').map(&:
 # pattern: to keep only pos: [3,2,1] -> "###.##.#"
 # string: clean up leading/trailing dots, compress runs of dots to one
 td1 = td.map {|p| [p[0].gsub(%r{\.*$|^\.*}, '').gsub(%r{\.+}, '.'), p[1].map {|n| "#" * n}.join(".")]}
-# td1.each {|p| puts "#{p[0]} vs #{p[1]}"}
 
-# puts numVariants("?.?.?.#", 0, "#.#", 0)
-# puts numVariants("???.###", 0, "#.#.###", 0)
-# puts numVariants("??.??.?##", 0, "#.#.###", 0)
-# puts numVariants("?###????????", 0, "###.##.#", 0)
-puts td1.map {|p| numVariants(p[0], 0, p[1], 0) }.sum()
+# part1
+# td1.each {|p| puts "#{p[0]} vs #{p[1]}: #{numVariants(p[0], 0, p[1], 0, {})}"}
+puts td1.map {|p| numVariants(p[0], 0, p[1], 0, {}) }.sum()
 
-# 5 times more
+# part2: 5 times more, needs memoization
 td2 = td.map {|p| [([p[0]]*5).join('?'), p[1]*5] }.map {|p| [p[0].gsub(%r{\.*$|^\.*}, '').gsub(%r{\.+}, '.'), p[1].map {|n| "#" * n}.join(".")]}
-td2.each {|p| puts "#{p[0]} vs #{p[1]}: #{numVariants(p[0], 0, p[1], 0)}"}
-puts td2.map {|p| numVariants(p[0], 0, p[1], 0) }.sum()
+# td2.each {|p| puts "#{p[0]} vs #{p[1]}: #{numVariants(p[0], 0, p[1], 0, {})}"}
+puts td2.map {|p| numVariants(p[0], 0, p[1], 0, {}) }.sum()
