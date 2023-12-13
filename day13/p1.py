8a369fef1fd780d718ad23909fa2eef7a1520d20
Alexey Vekshin <alexei.vekshin@gmail.com>
2023-12-13 22:28:55 +0000

    add mirror finder; d13.p1 ok

--- day13/p1.py
+++ day13/p1.py
@@ -12,6 +12,17 @@ def rotateMaze(maze: list[str]) -> list[str]:
       rot[cnum] += char
   return rot
 
-print("\n".join(mazes[0]))
-print()
-print("\n".join(rotateMaze(mazes[0])))
+def findMirr(maze: list[str]) -> int:
+  for lineno in range(0, len(maze)-1):
+    if all(maze[lineno-offset] == maze[lineno+offset+1] for offset in range(0, min(lineno+1, len(maze)-lineno-1))):
+      return lineno
+  return -1
+
+# print("\n".join(mazes[0]))
+# print(findMirr(mazes[0]))
+# print()
+# print("\n".join(rotateMaze(mazes[0])))
+# print(findMirr(rotateMaze(mazes[0])))
+
+ans1 = sum(100*(findMirr(m) + 1) + (findMirr(rotateMaze(m))+1) for m in mazes)
+print("ans1:", ans1)
