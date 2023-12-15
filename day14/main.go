ba9f252e9129c368d65b7c4de8107ce19ca03050
Alexey Vekshin <alexei.vekshin@gmail.com>
2023-12-15 09:19:55 +0000

    d14.p2: impl spin

--- day14/main.go
+++ day14/main.go
@@ -33,7 +33,7 @@ func readData(fh *os.File) [][]byte {
 	return res
 }
 
-func solve(field [][]byte) int {
+func solve1(field [][]byte) int {
 	res := 0
 	for c := 0; c < len(field[0]); c++ {
 		lastO := -1
@@ -60,9 +60,116 @@ func solve(field [][]byte) int {
 	return res
 }
 
+func spin(field [][]byte) int {
+	// north
+	for c := 0; c < len(field[0]); c++ {
+		lastO := -1
+		for r := 0; r < len(field); r++ {
+			sym := field[r][c]
+			switch sym {
+			case 'O':
+				newPos := r
+				if newPos > lastO + 1 {
+					newPos = lastO + 1
+					field[newPos][c] = 'O'
+					field[r][c] = '.'
+				}
+				lastO = newPos
+			case '#':
+				lastO = r
+			case '.':
+				continue
+			}
+		}
+	}
+	// lo.ForEach(field, func (r []byte, i int) {fmt.Printf("north %d: %v\n", i, string(r))})
+	// west
+	for r := 0; r < len(field); r++ {
+		lastC := -1
+		for c := 0; c < len(field[0]); c++ {
+			sym := field[r][c]
+			switch sym {
+			case 'O':
+				newPos := c
+				if newPos > lastC + 1 {
+					newPos = lastC + 1
+					field[r][newPos] = 'O'
+					field[r][c] = '.'
+				}
+				lastC = newPos
+			case '#':
+				lastC = c
+			case '.':
+				continue
+			}
+		}
+	}
+	// lo.ForEach(field, func (r []byte, i int) {fmt.Printf("west %d: %v\n", i, string(r))})
+	// south
+	for c := 0; c < len(field[0]); c++ {
+		lastO := len(field)
+		for r := len(field)-1; r >= 0; r-- {
+			sym := field[r][c]
+			switch sym {
+			case 'O':
+				newPos := r
+				if newPos < lastO - 1 {
+					newPos = lastO - 1
+					field[newPos][c] = 'O'
+					field[r][c] = '.'
+				}
+				lastO = newPos
+			case '#':
+				lastO = r
+			case '.':
+				continue
+			}
+		}
+	}
+	// lo.ForEach(field, func (r []byte, i int) {fmt.Printf("south %d: %v\n", i, string(r))})
+	// east
+	for r := 0; r < len(field); r++ {
+		lastC := len(field[0])
+		for c := len(field[0])-1; c >= 0; c-- {
+			sym := field[r][c]
+			switch sym {
+			case 'O':
+				newPos := c
+				if newPos < lastC - 1 {
+					newPos = lastC - 1
+					field[r][newPos] = 'O'
+					field[r][c] = '.'
+				}
+				lastC = newPos
+			case '#':
+				lastC = c
+			case '.':
+				continue
+			}
+		}
+	}
+	// lo.ForEach(field, func (r []byte, i int) {fmt.Printf("east %d: %v\n", i, string(r))})
+	// load on north
+	res := 0
+	for c := 0; c < len(field[0]); c++ {
+		for r := 0; r < len(field); r++ {
+			if field[r][c] == 'O' {
+				res += len(field) - r
+			}
+		}
+	}
+	return res
+}
+
 func main() {
 	platform := readData(getFH("input.txt"))
 	lo.ForEach(platform, func (r []byte, i int) {fmt.Printf("%d: %v\n", i, string(r))})
 	fmt.Println("----")
-	fmt.Println("ans1:", solve(platform))
+	// fmt.Println("ans1:", solve1(platform))
+	for i := 1; i <= 5; i++ {
+		fmt.Println("# spin:", i)
+		load := spin(platform)
+		lo.ForEach(platform, func (r []byte, i int) {fmt.Printf("%d: %v\n", i, string(r))})
+		fmt.Println("- load:", load)
+	}
 }
