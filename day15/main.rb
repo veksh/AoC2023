493626c9220ab3ab5767ef77495010bee2015dd5
Alexey Vekshin <alexei.vekshin@gmail.com>
2023-12-15 15:35:29 +0000

    d15.p2 and day15 ok

--- day15/main.rb
+++ day15/main.rb
@@ -7,3 +7,26 @@ end
 steps = ARGF.read().strip().split(',')
 ans1 = steps.map {|s| HASH(s)}.sum()
 puts "ans1: #{ans1}"
+
+boxes = 255.times.map {[]}
+steps.each do |s|
+  label, fl = s.split(/[=-]/)
+  boxNo = HASH(label)
+  if fl != nil
+    if (pp = boxes[boxNo].index {|p| p[0] == label}) !=nil
+      boxes[boxNo][pp][1] = fl
+    else
+      boxes[boxNo].append([label, fl])
+    end
+  else
+    if (pp = boxes[boxNo].index {|p| p[0] == label}) !=nil
+      boxes[boxNo].delete_at(pp)
+    end
+  end
+end
+
+ans2 = boxes.each_with_index.map { |b, bNum|
+  b.each_with_index.map { |l, lPos|
+    (bNum + 1)*(lPos + 1)*(l[1].to_i) }.sum
+}.sum
+puts "ans2: #{ans2}"
