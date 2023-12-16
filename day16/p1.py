6c53f0564c304d69067d7511293d7f5499a59a68
Alexey Vekshin <alexei.vekshin@gmail.com>
2023-12-16 13:19:31 +0000

    d16.p1 done

--- day16/p1.py
+++ day16/p1.py
@@ -4,11 +4,35 @@ import sys
 import typing
 
 maze = [[c for c in l] for l in open(sys.argv[1] if len(sys.argv) > 1 else 0).read().splitlines()]
-print("maze:\n", "\n".join([''.join(l) for l in maze]))
+print("\n".join([''.join(l) for l in maze]))
 
-DNUM = "RDLU"
-DYDX  = [[0, 1], [1, 0], [0, -1], [-1, 0]] # DYDX[DNUM.index(d)]
+DYDX = {'R': [0, 1], 'D': [1, 0], 'L': [0, -1], 'U': [-1, 0]}
+REFL = {
+  "\\": {'R': 'D', 'L': 'U', 'D': 'R', 'U': 'L'},
+  "/" : {'R': 'U', 'L': 'D', 'D': 'L', 'U': 'R'}
+}
 
-beams = [[0, 0, 0]] # y, x, dir
-
-# while len(beams) > 0:
+beams = [(0, -1, 'R')] # row, col, dir
+energized = set() # set(beam)
+while len(beams) > 0:
+  newbeams = []
+  for r, c, d in beams:
+    r, c = r + DYDX[d][0], c + DYDX[d][1]
+    if r < 0 or r >= len(maze) or c < 0 or c >= len(maze[0]):
+      continue
+    if tuple([r, c, d]) in energized:
+      continue
+    energized.add(tuple([r, c, d]))
+    sym = maze[r][c]
+    if sym in '/\\':
+      newbeams.append((r, c, REFL[sym][d]))
+    elif sym == '|' and d in 'RL':
+      newbeams.append((r, c, 'U'))
+      newbeams.append((r, c, 'D'))
+    elif sym == '-' and d in 'UD':
+      newbeams.append((r, c, 'L'))
+      newbeams.append((r, c, 'R'))
+    else:
+      newbeams.append((r, c, d))
+  beams = newbeams
+print(len(set((p[0], p[1]) for p in energized)))
