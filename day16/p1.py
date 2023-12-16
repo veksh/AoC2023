c359e369cf8ddbdd5dc63d9ebb555abd40e17053
Alexey Vekshin <alexei.vekshin@gmail.com>
2023-12-16 11:58:42 +0000

    finer maze split

--- day16/p1.py
+++ day16/p1.py
@@ -3,5 +3,12 @@
 import sys
 import typing
 
-maze = open(sys.argv[1] if len(sys.argv) > 1 else 0).read().splitlines()
-print("maze:\n", "\n".join(maze))
+maze = [[c for c in l] for l in open(sys.argv[1] if len(sys.argv) > 1 else 0).read().splitlines()]
+print("maze:\n", "\n".join([''.join(l) for l in maze]))
+
+DNUM = "RDLU"
+DYDX  = [[0, 1], [1, 0], [0, -1], [-1, 0]] # DYDX[DNUM.index(d)]
+
+beams = [[0, 0, 0]] # y, x, dir
+
+# while len(beams) > 0:
