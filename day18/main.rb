951aaf5300f012e957277cc5154387ba2521e701
Alexey Vekshin <alexei.vekshin@gmail.com>
2023-12-20 23:24:46 +0000

    approximate size

--- day18/main.rb
+++ day18/main.rb
@@ -1,4 +1,7 @@
 #!/usr/bin/ruby
 
+# "dir" steps "#color"
 steps = ARGF.readlines().map {|l| l.split(' ')}.map {|a| [a[0], a[1].to_i, a[2][1..-2]]}
 puts "#{steps}"
+sums = steps.reduce(Hash.new(0)) {|msum, m| msum[m[0]] += m[1]; msum }
+puts "#{sums}"
