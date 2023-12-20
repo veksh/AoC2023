c6aea0a2e37e537e5ab80c15bdf25ce620e9949b
Alexey Vekshin <alexei.vekshin@gmail.com>
2023-12-20 11:15:09 +0000

    d17.p1: first attempt at maze solver

--- day17/main.go
+++ day17/main.go
@@ -32,8 +32,69 @@ func readData(fh *os.File) [][]int {
 	return res
 }
 
+// row, col; RDLU
+var RDLU = [][2]int {
+	{ 0,  1}, // R 0
+	{ 1,  0}, // D 1
+	{ 0, -1}, // L 2
+	{-1,  0}, // U 3
+}
+
+type cell struct {
+	row  int
+	col  int
+	cost int
+	dir  byte
+	run  byte
+}
+
+const MAXRUN = 3
+var VOID = struct{}{}
+
 func solve1(field [][]int) int {
-	return len(field)
+	maxR, maxC := len(field)-1, len(field[0])-1
+	seen := map[cell]struct{}{}
+	queue := []cell{
+		{row: 0, col: 0, dir: 0, run: 0, cost: -1*field[0][0]},
+		{row: 0, col: 0, dir: 2, run: 0, cost: -1*field[0][0]},
+	}
+	res := 0
+	for len(queue) > 0 {
+		newq := []cell{}
+		for _,c := range(queue) {
+			if c.row < 0 || c.row > maxR || c.col < 0 || c.col > maxC {
+				continue
+			}
+			c.cost += field[c.row][c.col]
+			if _, ok := seen[c]; ok {
+				continue
+			}
+			seen[c] = VOID
+			if c.row == maxR && c.col == maxR {
+				res = max(res, c.cost)
+			}
+			if c.run <= MAXRUN {
+				newq = append(newq, cell{
+					row: c.row + RDLU[c.dir][0],
+					col: c.col + RDLU[c.dir][1],
+					cost: c.cost,
+					dir: c.dir,
+					run: c.run + 1,
+				})
+			}
+			for _, turn := range([]byte{(c.dir + 1) % 4, (c.dir + 3) % 4}) {
+				newq = append(newq, cell{
+						row: c.row + RDLU[turn][0],
+						col: c.col + RDLU[turn][1],
+						cost: c.cost,
+						dir: turn,
+						run: 0,
+				})
+			}
+		}
+		queue = newq
+	}
+	return res
 }
 
 func main() {
