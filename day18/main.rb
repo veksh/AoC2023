903a185c42b7348ad5c1eccd0a4cc858e3db9f6f
Alexey Vekshin <alexei.vekshin@gmail.com>
2023-12-21 08:07:33 +0000

    min-max pos calculation

--- day18/main.rb
+++ day18/main.rb
@@ -2,6 +2,19 @@
 
 # "dir" steps "#color"
 steps = ARGF.readlines().map {|l| l.split(' ')}.map {|a| [a[0], a[1].to_i, a[2][1..-2]]}
-puts "#{steps}"
+# puts "#{steps}"
+
 sums = steps.reduce(Hash.new(0)) {|msum, m| msum[m[0]] += m[1]; msum }
 puts "#{sums}"
+
+RLUD = {"R" => [0, 1], "L" => [0, -1], "U" => [-1, 0], "D" => [1, 0]}
+
+path = steps.reduce([[0,0]]) {|sofar, step|
+  p = sofar[-1].clone()
+  p[0] += step[1]*RLUD[step[0]][0]
+  p[1] += step[1]*RLUD[step[0]][1]
+  sofar.push(p)
+}
+# puts "#{path}"
+puts "minr: #{path.map{|p| p[0]}.min()}, maxr: #{path.map{|p| p[0]}.max()}"
+puts "minc: #{path.map{|p| p[1]}.min()}, maxc: #{path.map{|p| p[1]}.max()}"
