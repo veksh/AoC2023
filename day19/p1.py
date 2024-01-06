9b72be0b8353461f0b2f7dfb18078dd62ab5a93d
Alexey Vekshin <alexei.vekshin@gmail.com>
2024-01-06 21:18:16 +0000

    pretty-print parset data

--- day19/p1.py
+++ day19/p1.py
@@ -2,14 +2,23 @@
 
 import sys
 import re
+import pprint
 
 # class Stage:
-#  def __init__(self, line):
+#   def __init__(self, line):
+#     self.name, self.sink = re.findall(r'^(\w+)\{.*,(\w+)\}$', l)[0]
+#     stages.ops = []
+#     for m in re.findall(r'([xmas])([<>])(\d+):(\w+)', l):
+#       stages[name]["ops"].append((m[0], m[1], int(m[2]), m[3]))
+
+#   def __repr__(self):
+#     return "stage %s"
 
 slines, plines = open(sys.argv[1] if len(sys.argv) > 1 else 0).read().rstrip("\n").split("\n\n")
 
 parts = [{k: int(v) for k,v in re.findall(r'([xmas])=(\d+)[,}]', l)} for l in plines.split("\n")]
-print(parts)
+pprint.pprint(parts)
+
 
 stage0 = re.search(r'^\w+', slines[0])[0]
 stages = {}
@@ -19,4 +28,5 @@ for l in slines.split("\n"):
   stages[name]["ops"] = []
   for m in re.findall(r'([xmas])([<>])(\d+):(\w+)', l):
     stages[name]["ops"].append((m[0], m[1], int(m[2]), m[3]))
-print(stages)
+for n,s in stages.items():
+  print("stage %s: %s" % (n, s))
