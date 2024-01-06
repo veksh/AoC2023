6fa00dc734962a668c6f5a8c91c18cc3c2e192cc
Alexey Vekshin <alexei.vekshin@gmail.com>
2024-01-06 14:08:21 +0000

    redone parsing

--- day19/p1.py
+++ day19/p1.py
@@ -3,13 +3,20 @@
 import sys
 import re
 
-slines, plines = open(sys.argv[1] if len(sys.argv) > 1 else 0).read().rstrip("\n").split("\n\n")
+# class Stage:
+#  def __init__(self, line):
 
-#  re.findall(r'([xmas])([<>])(\d+):(\w+)', s1)
-#  re.findall(r'^(\w+)\{.*,(\w+)\}$', s1)
-#  re.findall(r'([xmas])=(\d+)[,}]', s2)
+slines, plines = open(sys.argv[1] if len(sys.argv) > 1 else 0).read().rstrip("\n").split("\n\n")
 
 parts = [{k: int(v) for k,v in re.findall(r'([xmas])=(\d+)[,}]', l)} for l in plines.split("\n")]
 print(parts)
-stages = [re.findall(r'^(\w+)\{.*,(\w+)\}$', l) + re.findall(r'([xmas])([<>])(\d+):(\w+)', l) for l in slines.split("\n")]
+
+stage0 = re.search(r'^\w+', slines[0])[0]
+stages = {}
+for l in slines.split("\n"):
+  name, sink = re.findall(r'^(\w+)\{.*,(\w+)\}$', l)[0]
+  stages[name] = {"sink": sink}
+  stages[name]["ops"] = []
+  for m in re.findall(r'([xmas])([<>])(\d+):(\w+)', l):
+    stages[name]["ops"].append((m[0], m[1], int(m[2]), m[3]))
 print(stages)
