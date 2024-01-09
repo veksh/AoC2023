ea0cce9ca62f304f0ff166b737d82fa71359139f
Alexey Vekshin <alexei.vekshin@gmail.com>
2024-01-09 12:14:39 +0000

    construct objects out of gates

--- day20/main.go
+++ day20/main.go
@@ -11,9 +11,14 @@ import (
 
 type gate struct {
 	name string
-	kind byte
-	inputs  []string
+	kind string
+	states  map[string]int
 	outputs []string
+}
+
+func (g gate)String() string {
+	return fmt.Sprintf("%s: %s, outs %v, states %v",
+		g.name, g.kind, g.outputs, g.states)
 }
 
 func main() {
@@ -21,6 +26,7 @@ func main() {
 	if err != nil {log.Fatalf("cannot open: %v", err)}
 	defer fh.Close()
 	lr := bufio.NewReader(fh)
+	gates := []gate{}
 	for {
 		str, err := lr.ReadString('\n')
 		if err != nil {
@@ -28,6 +34,8 @@ func main() {
 		}
 		parts := strings.Split(strings.Trim(strings.Replace(str, ",", "", -1), "\n"), " ")
 		kind, name, outs := string(parts[0][0]), parts[0][1:], parts[2:]
-		fmt.Printf("%s: %s, outs %v\n", name, kind, outs)
+		gate := gate{name: name, kind: kind, outputs: outs, states: map[string]int{}}
+		fmt.Println(gate)
+		gates = append(gates, gate)
 	}
 }
