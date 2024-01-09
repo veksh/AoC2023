5478db26540381a1809c009fbf88427ec7696fe2
Alexey Vekshin <alexei.vekshin@gmail.com>
2024-01-09 17:18:30 +0000

    first attempt to process pulses

--- day20/main.go
+++ day20/main.go
@@ -6,8 +6,10 @@ import (
 	"log"
 	"os"
 	"strings"
+
 	// "github.com/samber/lo"
 	"github.com/davecgh/go-spew/spew"
+	"github.com/samber/lo"
 )
 
 type Gate struct {
@@ -17,13 +19,18 @@ type Gate struct {
 	outputs []string
 }
 
-func (g Gate)String() string {
+type Signal struct {
+	gateName string
+	value    int
+}
+
+func (g Gate) String() string {
 	return fmt.Sprintf("%s: %s, outs %v, states %v",
 		g.name, g.kind, g.outputs, g.state)
 }
 
 // 0 none, 1 high, -1 low
-func (g Gate)Signal(in string, val int) int {
+func (g Gate) Process(in string, val int) int {
 	switch g.kind {
 	case "%":
 		if val == 1 {
@@ -78,4 +85,26 @@ func main() {
 		}
 	}
 	spew.Dump(gates)
+	queue := []Signal{{"roadcaster", -1}}
+	lows, highs := 0, 0
+	for len(queue) > 0 {
+		h := lo.Reduce(queue, func(agg int, item Signal, _ int) int {return agg + (item.value + 1)/2}, 0)
+		l := len(queue) - h
+		spew.Printf("q: %v, highs +%d, lows +%d\n", queue, h, l)
+		highs, lows = highs + h, lows + l
+		newq := []Signal{}
+	 	for _, signal := range(queue) {
+	 		gate := gates[signal.gateName]
+	 		fmt.Printf(" processing signal %d from %s to %v\n", signal.value, signal.gateName, gate.outputs)
+	 		for _, oname := range(gate.outputs) {
+	 			res := gates[oname].Process(signal.gateName, signal.value)
+				if res != 0 {
+					fmt.Printf("  %s outputs %d\n", oname, res)
+					newq = append(newq, Signal{oname, res})
+				}
+			}
+		}
+		queue = newq
+	}
+	fmt.Printf("higs %d, lows %d, ans1 %d\n", highs, lows, highs*lows)
 }
