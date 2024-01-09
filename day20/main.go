5555fc818d5e59ac78db955f08768e0d76153fcb
Alexey Vekshin <alexei.vekshin@gmail.com>
2024-01-09 14:31:17 +0000

    add states to gates

--- day20/main.go
+++ day20/main.go
@@ -9,16 +9,16 @@ import (
 	// "github.com/samber/lo"
 )
 
-type gate struct {
+type Gate struct {
 	name string
 	kind string
-	states  map[string]int
+	state map[string]int
 	outputs []string
 }
 
-func (g gate)String() string {
+func (g Gate)String() string {
 	return fmt.Sprintf("%s: %s, outs %v, states %v",
-		g.name, g.kind, g.outputs, g.states)
+		g.name, g.kind, g.outputs, g.state)
 }
 
 func main() {
@@ -26,7 +26,7 @@ func main() {
 	if err != nil {log.Fatalf("cannot open: %v", err)}
 	defer fh.Close()
 	lr := bufio.NewReader(fh)
-	gates := []gate{}
+	gates := map[string]Gate{}
 	for {
 		str, err := lr.ReadString('\n')
 		if err != nil {
@@ -34,8 +34,18 @@ func main() {
 		}
 		parts := strings.Split(strings.Trim(strings.Replace(str, ",", "", -1), "\n"), " ")
 		kind, name, outs := string(parts[0][0]), parts[0][1:], parts[2:]
-		gate := gate{name: name, kind: kind, outputs: outs, states: map[string]int{}}
+		gate := Gate{name: name, kind: kind, outputs: outs, state: map[string]int{}}
 		fmt.Println(gate)
-		gates = append(gates, gate)
+		gates[name] = gate
 	}
+	for gname, gate := range(gates) {
+		for _, oname := range(gate.outputs) {
+			if outg, ok := gates[oname]; ok {
+				outg.state[gname] = -1
+			} else {
+				gates[oname] = Gate{name: oname, kind: "s", state: map[string]int{}}
+			}
+		}
+	}
+	fmt.Println(gates)
 }
