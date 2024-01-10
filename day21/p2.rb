9c92999d3d686f39ef1b5eb6abfbc166100d5b20
Alexey Vekshin <alexei.vekshin@gmail.com>
2024-01-10 16:35:02 +0000

    split into p1 and p2, slight tweak to path finding

---
+++ day21/p2.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/ruby
+
+require "set"
+
+steps = ARGV.pop().to_i || 26501365 # 5*11*481843 steps
+maze = ARGF.readlines().map {|l| l.strip().chars()}
+
+sr = maze.find_index {|r| r.include? "S"}
+sc = maze[sr].find_index("S")
+puts "start: #{sr} #{sc}, moves: #{steps}"
+maze[sr][sc] = "."
+
+moves = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+
+seen = [Set.new(), Set.new()] # even and odd
+
+q = [[sr, sc]]
+(1..steps).each do |i|
+  puts "step #{i}, sizes: #{seen.map(&:length)}" if i % 1000 == 0
+  qnew = []
+  curr_seen = seen[i % 2]
+  q.each do |p|
+    moves.each do |move|
+      n = [p[0] + move[0], p[1] + move[1]]
+      if !curr_seen.include?(n) && maze[n[0]][n[1]] == "."
+        curr_seen.add(n)
+        qnew.push(n)
+      end
+    end
+  end
+  q = qnew
+end
+puts "ans2: #{seen[0].length()}"
