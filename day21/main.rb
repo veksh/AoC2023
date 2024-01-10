21e494d69379f7e3900743a260461cb7c9caa1d3
Alexey Vekshin <alexei.vekshin@gmail.com>
2024-01-10 13:06:41 +0000

    find start pos

--- day21/main.rb
+++ day21/main.rb
@@ -1,4 +1,8 @@
 #!/usr/bin/ruby
 
 maze = ARGF.readlines().map {|l| l.strip().chars()}
-puts "#{maze}"
+
+sr = maze.find_index {|r| r.include? "S"}
+sc = maze[sr].find_index("S")
+puts "start: #{sr} #{sc}"
+
