6137e6e08b8671f5044d6233fb7e31d26a49e6c2
Alexey Vekshin <alexei.vekshin@gmail.com>
2024-01-22 19:03:44 +0000

    intersect

--- day24/p1.rb
+++ day24/p1.rb
@@ -4,3 +4,7 @@ stones = ARGF.readlines().map do |l|
   Hash[%i(px py pz vx vy vz).zip(l.split(/[,@ ]+/).map(&:to_f))]
 end
 puts "#{stones}"
+a, b = stones[0], stones[1]
+t = ((b[:px] - a[:px])/b[:vx] + (a[:py] - b[:py])/b[:vy])/(a[:vx]/b[:vx] - a[:vy]/b[:vy])
+cx, cy = a[:vx]*t + a[:px], a[:vy]*t + a[:py]
+puts "#{cx}, #{cy}"
