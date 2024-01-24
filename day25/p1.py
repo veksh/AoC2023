dacc2ba36e762e512bf23bd0d5796337005d5725
Alexey Vekshin <alexei.vekshin@gmail.com>
2024-01-24 11:13:56 +0000

    first attemt at finding paths

--- day25/p1.py
+++ day25/p1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import sys
-from collections import defaultdict
+from collections import defaultdict, deque
 
 lines = [l.replace(":", "").split() for l in open(sys.argv[1]).read().rstrip("\n").splitlines()]
 print(lines)
@@ -13,3 +13,22 @@ for l in lines:
     nodes[p].add(n)
 for n, edges in nodes.items():
   print("%s: vals %s" % (n, edges))
+
+
+def findPathEnds(nodes, s, f):
+  ans = []
+  visited = set([s])
+  queue = deque([s])
+  while len(queue) > 0:
+    e = queue.pop()
+    for n in nodes[e]:
+      if n in visited:
+        continue
+      if n == f:
+        ans.append(e)
+        continue
+      visited.add(n)
+      queue.appendleft(n)
+  return ans
+
+print("paths:", findPathEnds(nodes, 'bvb', 'cmg'))
