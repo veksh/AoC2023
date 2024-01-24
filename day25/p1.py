071005ee4599c2a3bd97928c1e5558bd341be7c7
Alexey Vekshin <alexei.vekshin@gmail.com>
2024-01-24 16:10:18 +0000

    lighter on debug

--- day25/p1.py
+++ day25/p1.py
@@ -4,37 +4,36 @@ import sys
 from collections import defaultdict, deque
 
 lines = [l.replace(":", "").split() for l in open(sys.argv[1]).read().rstrip("\n").splitlines()]
-print(lines)
 nodes = defaultdict(set)
 for l in lines:
   n = l[0]
   for p in l[1:]:
     nodes[n].add(p)
     nodes[p].add(n)
-for n, edges in nodes.items():
-  print("%s: vals %s" % (n, edges))
+#for n, edges in nodes.items():
+#  print("%s: vals %s" % (n, edges))
 
 # lets find N independent paths from s to f (where N <= min(num of neighbours of s or f))
 # if N == 3, cutting them (by severing first edges leading from s) will split a graph
-def findPathEnds(nodes, s, f):
-  print("walking from %s to %s" % (s, f))
+def findPathStarts(nodes, s, f):
+  # print("walking from %s to %s" % (s, f))
   ans = set()
   usedEdges = set()
   for n in nodes[s]:
     visited = set([s])
-    print("trying", n, ", seen edges", usedEdges)
+    # print("trying", n, ", seen edges", usedEdges)
     usedEdges.add((s, n))
     queue = deque([n])
     parent = {n: s, s: None}
     while len(queue) > 0:
       e = queue.pop()
-      print(" looking at", e)
+      # print(" looking at", e)
       visited.add(e)
       if e == f:
-        print("  reached", f, "starting from", n, "parent", parent[e])
+        # print("  reached", f, "starting from", n, "parent", parent[e])
         ans.add(n)
         while parent[e] != s:
-          print("   via", parent[e])
+          # print("   via", parent[e])
           usedEdges.add((parent[e], e))
           e = parent[e]
         break
@@ -44,8 +43,10 @@ def findPathEnds(nodes, s, f):
         if tip in parent:
           continue
         parent[tip] = e
-        print("  adding to queue:", tip)
+        # print("  adding to queue:", tip)
         queue.appendleft(tip)
   return ans
 
-print("starts:", findPathEnds(nodes, 'cmg', 'bvb'))
+print("*** starts for cmg->bvb:", findPathStarts(nodes, 'cmg', 'bvb'))
+print("*** starts for cmg->frs:", findPathStarts(nodes, 'cmg', 'frs'))
+
